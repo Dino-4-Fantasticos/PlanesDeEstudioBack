93925873cb218d168bf529672f222a85c949c26c
Diego Estrada Talamantes <devdiegoest@gmail.com>
2021-04-19 13:39:38 -0600

    Implementación rutas y pruebas de CREATE de planes de estudio

--- server.js
+++ server.js
@@ -8,4 +8,7 @@ app.use(express.json());
 const usersRouter = require("./src/users/router");
 app.use("/api/users", usersRouter);
 
+const planesRouter = require("./src/planes/router");
+app.use("/api/planes", planesRouter);
+
 module.exports = app;
