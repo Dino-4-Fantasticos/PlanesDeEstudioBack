c5e68ec7b4b54aec316d9239c760326d396f05a1
Diego Estrada Talamantes <devdiegoest@gmail.com>
2021-04-26 11:32:58 -0600

    Agregar front-end al repositorio

--- start.js
+++ start.js
@@ -1,4 +1,6 @@
 require("dotenv").config();
+const express = require("express");
+const path = require("path");
 
 const mongoose = require("mongoose");
 const app = require("./server.js");
@@ -12,5 +14,13 @@ mongoose
   })
   .then(() => console.log("MongoDB database connected successfully"))
   .catch(console.log);
+
+// Serve static assets if in production
+if (process.env.NODE_ENV === "production") {
+  app.use(express.static(path.join(__dirname, "build")));
+  app.get("*", (_, res) => {
+    res.sendFile(path.join(__dirname, "build", "index.html"));
+  });
+}
 
 app.listen(port, () => console.log(`Server is running on port: ${port}`));
