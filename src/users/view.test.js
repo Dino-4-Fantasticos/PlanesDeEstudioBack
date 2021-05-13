15124de5440d261e1cfece199a5ce144d1a1c5cf
Luis Alberto Pérez Chapa <luispc111@yahoo.com>
2021-05-12 21:15:16 -0500

    cambié identación

--- src/users/view.test.js
+++ src/users/view.test.js
@@ -4,9 +4,9 @@ import { render, screen } from "@testing-library/react";
 import Users from "./view";
 
 it("renderea texto de la vista", () => {
-    render(<Users />);
+  render(<Users />);
 
-    const admin = screen.getByText(/Administrador:/);
-  
-    expect(admin).toBeInTheDocument();
-  });
+  const admin = screen.getByText(/Administrador:/);
+ 
+  expect(admin).toBeInTheDocument();
+});
