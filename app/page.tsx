e82a25c381602f33478b5750f9e4e929ff02be55
alegayndra <alegayndra@gmail.com>
2023-08-01 13:59:39 -0600

    Vista de usuarios funcionando

--- app/page.tsx
+++ app/page.tsx
@@ -10,7 +10,7 @@ export default function Home() {
             Planes de Estudio
           </button>
         </Link>
-        <Link href="/usuarios" className="col-8 col-md-5 m-3">
+        <Link href="/users" className="col-8 col-md-5 m-3">
           <button className="btn btn-info btn-lg p-4">
             Usuarios y Administradores
           </button>
