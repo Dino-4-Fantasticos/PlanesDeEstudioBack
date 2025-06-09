fdc78fff9d096002b095fdcf179ccbdcae6546cf
alegayndra <alegayndra@gmail.com>
2025-06-08 20:50:46 -0600

    Fixed docker db config

--- start.js
+++ start.js
@@ -8,8 +8,8 @@ const app = require("./server.js");
 const port = process.env.PORT || 5000;
 
 const config = {
-  database: process.env.ATLAS_URI,
-  dropDatabase: true,
+	database: process.env.ATLAS_URI,
+	dropDatabase: true,
 };
 const seeder = new Seeder(config);
 const collections = seeder.readCollectionsFromPath(path.resolve("./seeds"));
@@ -27,29 +27,39 @@ const collections = seeder.readCollectionsFromPath(path.resolve("./seeds"));
 //     console.error('error con seeds db', err);
 //   });
 
-function connectDB() {
+const {
+	// MONGO_USERNAME,
+	// MONGO_PASSWORD,
+	MONGO_HOSTNAME,
+	MONGO_PORT,
+	MONGO_DB
+} = process.env;
 
-	console.log('connecting to DB', process.env.ATLAS_URI)
+function connectDB() {
+	// const url = `mongodb://${MONGO_USERNAME}:${MONGO_PASSWORD}@${MONGO_HOSTNAME}:${MONGO_PORT}/${MONGO_DB}?authSource=admin`;
+	const url = `mongodb://${MONGO_HOSTNAME}:${MONGO_PORT}/${MONGO_DB}`;
+
+	console.log('connecting to DB', url)
 	mongoose
-  		.connect(process.env.ATLAS_URI, {
-    		useNewUrlParser: true,
-    		useUnifiedTopology: true,
+		.connect(url, {
+			useNewUrlParser: true,
+			useUnifiedTopology: true,
     		// useCreateIndex: true,
-  	})
-  		.then(() => console.log("MongoDB database connected successfully"))
-  		.catch(err => console.log(err))
-  		.finally(() => console.log("finally"));
+	})
+		.then(() => console.log("MongoDB database connected successfully"))
+		.catch(err => console.log(err))
+		.finally(() => console.log("finally"));
 }
 
 connectDB();
 
 // Serve static assets if in production
 if (process.env.NODE_ENV === "production") {
-  console.log("produccion wiii");
-  app.use(express.static(path.join(__dirname, "build")));
-  app.get("*", (_, res) => {
-    res.sendFile(path.join(__dirname, "build", "index.html"));
-  });
+	console.log("produccion wiii");
+	app.use(express.static(path.join(__dirname, "build")));
+	app.get("*", (_, res) => {
+		res.sendFile(path.join(__dirname, "build", "index.html"));
+	});
 }
 
 app.listen(port, () => console.log(`Server is running on port: ${port}`));
